61b65dfbfbdb9b3a0d250de4c4020296022d6c23
Habib0088 <sarkerbatch2k19@gmail.com>
2025-08-22 22:09:45 +0600

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-login').addEventListener('click', function(e){
+    e.preventDefault();
+    // Set default value
+
+    let defaultMobile=123456789;
+    let defaultPassword=55555;
+
+    let mobileInput=document.getElementById('mobile').value;
+    let mobileInputInt=parseInt(mobileInput);
+
+    let passwordInput=document.getElementById('password').value;
+    let passwordInputInt=parseInt(passwordInput);
+
+    if(defaultMobile=== mobileInputInt && defaultPassword === passwordInputInt){
+       window.location.href='../home.html';
+       
+    }
+    else{
+        console.log('not matched');
+        
+    }
+    
+})
